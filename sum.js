f2652477a60fecb9d0ad7495fab389c4345c2b9a
ADMIN <admin@Ghostfacekiller.local>
2024-11-22 18:19:20 +0000

    first_commit

---
+++ sum.js
@@ -0,0 +1,12 @@
+const x1 = 10;
+const x2 = 20;
+
+const sum = x1 + x2;
+
+const x = 25;
+
+if (sum>x) {
+    console.log(`La somme (${sum}) est supérieur à ${x}.`);
+} else {
+    console.log(`La somme (${sum}) n est pas supérieur à ${x}.`);
+}
