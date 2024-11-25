bff87906e6f0f77c05642dcad2fb9ce93a81162a
ADMIN <admin@Ghostfacekiller.local>
2024-11-25 20:12:47 +0000

    assignment2 OK

---
+++ assignment-2/src/main/java/ai/ednova/assignment_2/controller/HealthController.java
@@ -0,0 +1,13 @@
+package ai.ednova.assignment_2.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Health Check OK");
+    }
+}
